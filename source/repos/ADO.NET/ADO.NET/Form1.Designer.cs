0d01bc1f7c37c2928fb0ed652a53d16113c64fc9
hieudinh1207 <66762798+hieudinh1207@users.noreply.github.com>
2021-06-22 21:23:44 +0700

    message

---
+++ source/repos/ADO.NET/ADO.NET/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ADO.NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridview1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gridview1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridview1
+            // 
+            this.gridview1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridview1.Location = new System.Drawing.Point(52, 123);
+            this.gridview1.Name = "gridview1";
+            this.gridview1.RowHeadersWidth = 51;
+            this.gridview1.RowTemplate.Height = 24;
+            this.gridview1.Size = new System.Drawing.Size(636, 303);
+            this.gridview1.TabIndex = 0;
+            this.gridview1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datagridview1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "List of People";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(152, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gridview1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridview1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridview1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ source/repos/ADO.NET/ADO.NET/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ADO.NET
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string sql_str;
+            string con_string = System.Configuration.ConfigurationManager.ConnectionStrings["TTTH"].ConnectionString;
+            SqlConnection cnn = new SqlConnection(con_string);
+            cnn.Open();
+            MessageBox.Show("Connection thanh cong!");
+            sql_str = "SELECT * FROM ADO_TEST.dbo.VITRI";
+            SqlCommand command = new SqlCommand(sql_str, cnn);
+            //command.CommandType = CommandType.Text;
+            SqlDataAdapter sda = new SqlDataAdapter(command);
+           DataTable dt = new DataTable();
+            sda.Fill(dt);
+            gridview1.DataSource = dt;
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            comboBox1.DataSource = ds.Tables[0];
+            comboBox1.DisplayMember = "TenViTri";
+            comboBox1.ValueMember = "ID";
+
+            cnn.Close();
+        }
+
+        private void datagridview1(object sender, DataGridViewCellEventArgs e)
+        {       
+          
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sql_str;
+            string con_string = System.Configuration.ConfigurationManager.ConnectionStrings["TTTH"].ConnectionString;
+            SqlConnection cnn = new SqlConnection(con_string);
+            cnn.Open();
+            SqlCommand Comman = new SqlCommand("SELECT ID,TenViTri FROM ADO_TEST.dbo.VITRI", cnn);
+            SqlDataAdapter sda = new SqlDataAdapter(Comman);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            comboBox1.DataSource = ds.Tables[0];
+            comboBox1.DisplayMember = "TenViTri";
+            comboBox1.ValueMember = "ID";
+            cnn.Close();
+        }
+    }
+}
